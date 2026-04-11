fdcba1394d9336dab44b9106052ece8fbbcdd5d8
NRG55 <maloj_1980@yahoo.fr>
2026-04-11 16:42:31 +0100

    feat(backend): add location validation logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,36 @@ app.get('/', (req, res) => {
     res.send("Where is Waldo API");
 });
 
+app.post('/validate-location', async (req, res) => {
+    const { gameId, characterName, x, y } = req.body;
+    const margin = 3; // 3% character coordinates margin
+
+    try {
+        const character = await prisma.character.findFirst({
+            where: {
+                gameId: parseInt(gameId),
+                name: { equals: characterName }
+            }
+        });
+
+        if (!character) {
+            return res.status(404).json({ message: "Character not found" });
+        };
+
+        const isFound = (x - character.xCoordinate) <= margin 
+                     && (y - character.yCoordinate) <= margin;
+
+        if (isFound) {           
+            return res.json({ found: true, message: `You found ${character.name}!` });
+        };
+        
+        res.json({ found: false, message: "Wrong character, keep looking!" });
+
+    } catch (error) {
+        res.status(500).json({ error: "Something wrong on a server side" });
+    };   
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

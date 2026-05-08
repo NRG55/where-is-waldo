217206deb9665f12860a5475706b87b2856d5230
NRG55 <maloj_1980@yahoo.fr>
2026-05-08 13:49:32 +0100

    feat(backend): add space searchable characters, add seed data example file

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,7 +1,7 @@
 import { Pool} from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/index.js';
-import { beachCharacters, skiCharacters } from './seedData.js';
+import { beachCharacters, skiCharacters, spaceCharacters } from './seedData.js';
 import 'dotenv/config';
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
@@ -40,11 +40,7 @@ async function main() {
             slug: "space",
             imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778156994/space_ytzhlk.jpg",
             characters: {
-                create: [
-                    { name: "Waldo", imageUrl: "", xCoordinate: 0, yCoordinate: 0 },
-                    { name: "Wizard", imageUrl: "", xCoordinate: 0, yCoordinate: 0 },
-                    { name: "Wenda", imageUrl: "", xCoordinate: 0, yCoordinate: 0 }
-                ]
+                create: spaceCharacters
             }
         }
     });
---
+++ backend/prisma/seedData.example.js
@@ -0,0 +1,62 @@
+export const beachCharacters = [
+    { 
+        name: "Wilfrid", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778159984/wilfrid_zlbg3j.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Willy", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778159990/willy_vdfkhm.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Wendell", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778159972/wendell_atrmio.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    }
+];
+
+export const skiCharacters = [
+    { 
+        name: "Yasemin", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778241594/yasemin_xmanvz.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Tyler", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778241585/tyler_batkfg.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Tiffany", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778241574/tiffany_wsbj5t.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    }
+];
+
+export const spaceCharacters = [
+    { 
+        name: "Zack", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778244126/zack_ktzxfs.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Brandon", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778244108/brandon_umwt99.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    },
+    { 
+        name: "Joshua", 
+        imageUrl: "https://res.cloudinary.com/dlc8atazj/image/upload/v1778244113/joshua_jmxfh6.jpg", 
+        xCoordinate: 0.00, 
+        yCoordinate: 0.00 
+    }
+]
